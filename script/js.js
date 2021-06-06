6bca141904e37ddec54180c3f65db41ac7543c3d
MaksymSydorovych <71548115+MaksymSydorovych@users.noreply.github.com>
2021-06-06 22:12:46 +0200

    1

---
+++ script/js.js
@@ -0,0 +1,2 @@
+document.querySelector('.burger').addEventListener ('click', () => document.querySelector('#menu').style.display = "block");
+
--- script/script.js
+++ script/script.js
@@ -41,5 +41,7 @@ function validateEmail(email) {
     const regEx = /\S+@\S+\.\S+/;
     const patternMatches = regEx.test(email);
     return patternMatches;
-}
+};
+
+
 
